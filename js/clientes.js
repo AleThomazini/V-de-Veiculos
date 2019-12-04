0c529860cf3c96c28d4e0f2e1daeae293ed8b257
Matheus Batista <matheusmdbatista@gmail.com>
2019-12-04 17:43:13 -0300

    carrinho com metodos prototipados

--- js/clientes.js
+++ js/clientes.js
@@ -1,4 +1,6 @@
+let itensLista = [];
 $(document).ready(function(){
+    itensLista = [];
     $.get("http://localhost:3000/imagens/", function (imgs) {
         let url;
         for (let index = 0; index < imgs.length; index++) {
@@ -6,6 +8,21 @@ $(document).ready(function(){
             listarVeiculosImgs(index, url);
         }
     });
+
+    $(document).on('click', 'input[name="increment"]', function(e) {
+      incrementValue(e);
+    });
+    
+    $(document).on('click','input[name="decrement"]', function(e) {
+      decrementValue(e);
+    }); 
+
+    $(document).on('click', 'button[name="removerItem"]', function(e) {
+      let index = $(e.currentTarget).data('value');
+      removerItemCarrinho(index);
+    });
+
+    adicionarItemCarrinho(1, 'audi.png', 'Nome Produto', 'Status Produto', 10, 1000000)
 })
 
 function listarVeiculosImgs(id, imagem) {
@@ -114,32 +131,73 @@ function incrementValue(e) {
     e.preventDefault();
     var fieldName = $(e.target).data('field');
     var parent = $(e.target).closest('div');
-    var currentVal = parseInt(parent.find('input[name=' + fieldName + ']').val(), 10);
-  
+    var index = $(e.target).data('value');
+    var currentVal = parseInt(parent.find('input[name=' + fieldName + ']').val(), 10) + 1;
+    var item = itensLista.find(e => e.index == index);
+    if (item) { 
+      item.quantidade = currentVal
+    }
     if (!isNaN(currentVal)) {
-      parent.find('input[name=' + fieldName + ']').val(currentVal + 1);
+      parent.find('input[name=' + fieldName + ']').val(currentVal);
     } else {
       parent.find('input[name=' + fieldName + ']').val(0);
     }
+
+    atualizarValores()
   }
   
   function decrementValue(e) {
     e.preventDefault();
     var fieldName = $(e.target).data('field');
     var parent = $(e.target).closest('div');
-    var currentVal = parseInt(parent.find('input[name=' + fieldName + ']').val(), 10);
-  
+    var index = $(e.target).data('value');
+    var currentVal = parseInt(parent.find('input[name=' + fieldName + ']').val(), 10) -1;
+    var item = itensLista.find(e => e.index == index);
+    if (item) { 
+      item.quantidade = currentVal
+    }
     if (!isNaN(currentVal) && currentVal > 0) {
-      parent.find('input[name=' + fieldName + ']').val(currentVal - 1);
+      parent.find('input[name=' + fieldName + ']').val(currentVal);
     } else {
       parent.find('input[name=' + fieldName + ']').val(0);
     }
+    atualizarValores();
+  } 
+
+  adicionarItemCarrinho = (index,imagem, nomeProduto, status, quantidade, valor) => {
+    itensLista.push({ 
+      index,
+      quantidade,
+      valor
+    })
+    const corpoTabela = $('#corpoTabela');
+    let html = (corpoTabela.html() ? corpoTabela.html() : "");
+    let tr = "<tr id='tr_"+index+"'>";
+    tr += '<td><img class="shopcar" src="img/'+ imagem +'" /> </td>';
+    tr += '<td>'+nomeProduto+'</td><td>'+status+'</td>'
+    tr += '<td><div class="input-group"><input type="button" value="-" class="button-minus" name="decrement" data-field="quantity" data-value="'+index+'"><input type="number" step="1" max="" value="'+quantidade+'" name="quantity" class="quantity-field"><input type="button" value="+" class="button-plus" data-field="quantity" name="increment" data-value="'+index+'"></div></td>';
+    tr += '<td class="text-right" id="valor">'+valor+'</td><td class="text-right"><button class="btn btn-sm btn-danger" name="removerItem" data-value="'+index+'"><i class="fa fa-trash"></i> </button> </td>';
+    tr += "</tr>"
+    html += tr;
+    corpoTabela.html(html);
+
+    atualizarValores();
   }
-  
-  $('.quantity').on('click', '.button-plus', function(e) {
-    incrementValue(e);
-  });
-  
-  $('.quantity').on('click', '.button-minus', function(e) {
-    decrementValue(e);
-  });
+
+  removerItemCarrinho = (index) => { 
+    let tr = $('#tr_' + index);
+    if (tr) {
+      tr.remove();
+      itensLista = itensLista.map(e => e.index != index);
+      atualizarValores();
+    }
+  }
+
+  atualizarValores = () => { 
+    let valores = itensLista.map(e => {
+     return e.quantidade * e.valor
+    })
+    let valor = valores.reduce((previous, currentValue) => previous + currentValue);
+    let html = '<tr><td colspan="5"><strong>Total</strong></td><td class="valor-total"><strong>R$</strong>'+ valor +'</td></tr>';
+    $('#rodapeTabela').html(html);
+  }
